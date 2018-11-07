1fa2cabdb596ce872ac62864fdf6b0c1cff6c532
Ricardo Palhares <ricardoapalhares@gmail.com>
2018-11-06 22:04:02 -0200

    exercicios

---
+++ aula03/estrutura_de_controle.py
@@ -0,0 +1,26 @@
+var = input("Digite um número: ")
+#if 10 > int(var):
+#	print("CONDIÇÃO VERDADEIRA", var + " é menor que 10")
+#else:
+#	print("CONDIÇÃO FALSA")
+
+numero = int(var)
+
+# if 10 > numero:
+# 	print(var + " é menor que 10")
+# elif 10 == numero:
+# 	pint(var + " é igual a 10")
+# else:
+# 	print(var + " é maior que 10")
+
+# x = 1
+# while x <= numero :
+# 	print(x)
+# 	x += 1
+
+
+
+#equilatero = todos iguais
+#dois lados iguais e um diferente
+#escaleno todos diferentes
+
