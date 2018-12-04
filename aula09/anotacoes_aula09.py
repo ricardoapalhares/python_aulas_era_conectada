5df1893cce06f3c3c801539aed5d8e585c3e9e99
Júnior <jrcjuniorcesar@gmail.com>
2018-12-04 21:54:18 -0200

    aula09

---
+++ aula09/anotacoes_aula09.py
@@ -0,0 +1,58 @@
+#Youtube
+#Python Brasil
+#Keynote - painel sobre dados abertos e tecnologias cívicas
+#brasil.io/datasets
+
+#https://pypi.org/project/pip/
+#https://pip.pypa.io/
+
+
+#pip install requests
+#pip list
+
+#import this
+
+#pip freeze
+
+#lista com a versao dos packages juntamente com versão
+#pip freeze > requirements.txt
+
+#instala a lista dos packages que estao dentro do arquivo requirements nas versoes indicadas
+#pip install -r requirements.txt
+
+#pip search oxente
+
+#https://github.com/typemytype/drawbot/blob/master/requirements.txt
+#quando não está no pip install
+#git+https://github.com/typesupply/vanilla
+
+#virtualenv
+#virtualenvwrapper
+#conda-forge
+	#https://conda-forge.org/
+	#gerenciar pacotes igual ao pip
+	#https://www.anaconda.com/download/
+
+#pyenv
+
+#pip install virtualenv
+#pip install virtualenvwrapper-win
+
+#cd .virtualenvs
+#ls
+
+#https://github.com/typemytype/drawbot
+
+#Criar um ambiente virtual
+# - p -> path (caminho)
+#mkvirtualenv desenho -p python 
+
+#sair do ambiente virtual
+#deactivate
+
+#entrar novamente
+#workon desenho
+
+
+
+
