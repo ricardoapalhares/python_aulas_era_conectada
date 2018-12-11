c9cd4a5d4a3c74177260033dca75396137d1bbd3
Júnior <jrcjuniorcesar@gmail.com>
2018-12-11 21:23:10 -0200

    aula 11

---
+++ aula11/anotacoes_aula11.py
@@ -0,0 +1,9 @@
+#virtualenv jupyter -p python
+#python -m pip install jupyter
+#jupyter notebook
+#escolhe a pasta e clica new python3
+#pip install numpy
+#pip install matplotlib
+#https://matplotlib.org/1.2.1/examples/api/span_regions.html
+#copy code cola no jupyter
+#file -> download as -> markdown (.MD)
