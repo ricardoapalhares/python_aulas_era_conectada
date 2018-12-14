9ea5dc780c514594594310b20cd792af1e4da52a
Júnior <jrcjuniorcesar@gmail.com>
2018-12-13 22:02:32 -0200

    aula django

---
+++ aula12/anotacoes_aula12.py
@@ -0,0 +1,28 @@
+#mkvirtualenv django -p python
+#pip install django
+#acompanhar instalacao do django
+#https://www.djangoproject.com/
+#https://docs.djangoproject.com/en/2.0/intro/tutorial01/
+#https://docs.djangoproject.com/en/2.1/intro/tutorial02/
+#https://opensource.com/business/16/8/intro-to-gource
+
+
+#django-admin startproject forum
+#cd forum
+#python manage.py runserver
+#http://127.0.0.1:8000/
+
+#python manage.py startapp polls
+#In the polls/urls.py file include the following code:
+#forum/urls.py
+#python manage.py runserver
+
+#http://rgth.co/
+#http://floripa.sc.python.org.br/encontros/
+
+#https://docs.djangoproject.com/en/2.1/ref/databases/
+
+#python manage.py migrate
+#python manage.py sqlmigrate polls 0001
+
+
