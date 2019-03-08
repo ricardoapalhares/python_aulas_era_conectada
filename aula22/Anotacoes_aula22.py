f91559a6a96dac62fa06be83d83d09e3ababd3d3
Júnior <jrcjuniorcesar@gmail.com>
2019-03-07 21:47:09 -0300

    Aula22

---
+++ aula22/Anotacoes_aula22.py
@@ -0,0 +1,38 @@
+# 07/03/2019 (quinta-feira)
+# 
+# 
+# 
+# virtualenv: Python Environment Manager
+# Cuida do ambiente virtual
+# 
+# pyenv: Python Version Manager
+# Cuida das versões do python
+# Ex. Projeto 3.5, mas preciso usar a 2.6
+# Com o pyenv instalo a versão 2.6 e uso o virtualenv para criar o ambiente virtual
+# 
+# https://github.com/pyenv/pyenv-installer
+# 
+# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+# 
+# pyenv install pyenv-win 
+# 
+# Entrar no Painel de Controle -> Variaveis de Ambiente -> Path -> 
+# C:\Users\python\.pyenv\pyenv-win\bin
+# 
+# 
+# pyenv install --list
+# pyenv install 3.7.2
+# 
+# python --version
+# 
+# pyenv global 3.6.8
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
