1039ff7eca8eee5a8a13b43cff95ad77126720ce
Júnior <jrcjuniorcesar@gmail.com>
2019-03-12 21:53:09 -0300

    aula 23

---
+++ aula23/Anotacoes_aula23.py
@@ -0,0 +1,51 @@
+# 12/03/2019 (terça-feira)
+# 
+# pyenv install --list
+#
+# pyenv install 3.6.4 
+# 
+# pyenv versions
+# pyenv global 3.6.4
+# pyenv local 3.6.4
+# mkvirtualenv test-win -p python
+# 
+# mkvirtualenv test-win2 -p c:\Users\python\.pyenv\pyenv-win\versions\3.5.0\python.exe
+# 
+# Web Scraping
+# import scrapy
+# 
+# using regular expression 
+# ([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})
+# 
+# 032.833.104-05
+# 014.255.558-99
+# 
+# Web crawling frameworks
+# Selenium
+# jQuery
+# Scrapy
+# Splash (simular javascript)
+# 
+# 
+# Busca que mostra diversas informações:
+# https://www.wolframalpha.com/
+# 
+# Exemplo de pesquisa: Brasil
+# 
+# Curso 
+# Python Web Scraping (udemy)
+# Evaldo Wolkers
+# 
+# Python pra zumbis
+# Fernando Masanori Ashikaga
+# PyCurso
+# 
+# Gileno Filho
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
