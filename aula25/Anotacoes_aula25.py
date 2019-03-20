dc48e6e13e300a7a063588b0474d6cf74cbb9545
Júnior <jrcjuniorcesar@gmail.com>
2019-03-19 21:41:25 -0300

    Aula 25

---
+++ aula25/Anotacoes_aula25.py
@@ -0,0 +1,31 @@
+
+# 19/03/2019 (terça-feira)
+# 
+# https://2019.pythonbrasil.org.br
+# 
+# Contabilizei
+# https://www.contabilizei.com.br/
+# https://www.gowork.com.br/escritorio-virtual/
+# 
+# Ambiente de Testes em Python
+# 
+# Para que testes?
+# 
+# raise NameError("Tá errado!")
+# 
+# pytest
+# 
+# mock
+# https://docs.python.org/3/library/unittest.mock.html
+# 
+# pi = mock.Mock()
+# pi.return_value = 3.14
+# pi()
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
---
+++ aula25/tests.py
@@ -0,0 +1,8 @@
+
+lista = range(100)
+
+for numero in lista:
+    if numero != None: 
+        print(numero)
+    else:
+        raise TypeError("Nulo")
