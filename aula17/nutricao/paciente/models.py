e663763d681c6a388b0344ab7e2a2011a7cc5dd2
Júnior <jrcjuniorcesar@gmail.com>
2019-03-28 22:03:30 -0300

    Aula 28

--- aula17/nutricao/paciente/models.py
+++ aula17/nutricao/paciente/models.py
@@ -2,20 +2,48 @@ from django.db import models
 
 class Paciente(models.Model):
     nome = models.CharField(max_length=200) 
-    idade = models.DateTimeField()
+    data_nascimento = models.DateTimeField()
     telefone = models.CharField(max_length=15)
     profissao = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+    sexo = models.CharField(max_length=20, choices=[('M','Masculino'),('F','Feminino')])
 
     def __str__(self):
         return self.nome
 
 class Dieta(models.Model):
-    PERIODO = (('1','Manhã'),('2','Tarde'),('3','Noite'))
-    
-    #paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    paciente = models.ManyToManyField(Paciente, related_name='dieta')
     plano_alimentar = models.CharField(max_length=200)
-    periodo = models.CharField(max_length=20, choices=PERIODO)
+    periodo = models.CharField(max_length=20, choices=[('1','Manhã'),('2','Tarde'),('3','Noite')])
 
-    
+class Consulta(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    nutricionista = models.ForeignKey(Nutricionista, on_delete=models.CASCADE)
+    dieta = models.ForeignKey(Dieta, on_delete=models.CASCADE)
+    avaliacao = models.ForeignKey(Avaliacao, on_delete=models.CASCADE)
+    data_hora = models.DateTimeField()
+    tipo = models.CharField(max_length=20, choices=[('1','Consulta'),('2','Retorno')])
+    local = models.CharField(max_length=200)
+
+class Avaliacao(models.Model):
+    peso = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    altura = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    local = models.CharField(max_length=200)
+
+class Nutricionista(models.Model):
+    nome = models.CharField(max_length=200) 
+    especialidades = models.CharField(max_length=20, choices=[('1','Funcional'),('2','Esportivo'),('3','Pediatria')])
+    email = models.EmailField(max_length=254)
+    telefone = models.CharField(max_length=15)
+    crn = models.CharField(max_length=30)
+
+
+# AVALIACAO
+ # peso
+ # altura
+
+# NUTRICIONISTA
+ # nome
+ # especialidade
+ # email
+ # telefone
+ # crn
---
+++ aula28/Anotacoes_aula28.py
@@ -0,0 +1,40 @@
+# 
+# 28/03/2019 (quinta-feira)
+# 
+# Django Forms
+# Form = Form()
+# 
+# git branch
+# branch é pra funcionalidade (organizar funcionalidade, corrigir bug)
+# 
+# git checkout -b views-api master
+# git branch
+# 
+# git commit -m "novas views"
+# 
+# git push -u origin views-api 
+# 
+# 
+# https://github.com/Era-conectada-python/aulas
+# https://github.com/Era-conectada-python/aulas/compare/views-api
+# 
+# pull request
+# merge
+# depois disso a versão master fica atualizada
+# git pull pra pegar a versão mais atual
+# 
+# fork faz uma copia do projeto
+# 
+# livro: Aperte F5
+# 
+# pip freeze
+# 
+# git checkout -b requirements master
+# git branch
+# 
+# git commit -m "arquivo requirements"
+# 
+# git push -u origin requirements
+# fazer o pull request la no site git-hub 
+# git checkout master
+# git pull
