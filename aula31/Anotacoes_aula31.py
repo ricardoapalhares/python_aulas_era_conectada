6ae114b414dce51f6854f6936815a6cf0ad891d9
Júnior <jrcjuniorcesar@gmail.com>
2019-04-09 21:58:11 -0300

    aula 31

---
+++ aula31/Anotacoes_aula31.py
@@ -0,0 +1,37 @@
+# 
+# 09/04/2019 (terça-feira)
+# 
+# Data Science (mini conda)
+# conda -h
+# conda list
+# conda create -n envconda python=3
+# 
+# activate envconda
+# deactivate
+# 
+# conda install jupyter
+# jupyter notebook
+# -> new -> python3
+# 
+# 
+# ipython
+# 
+#  
+# site usado pela comunidade de datascience
+# scikit-learn
+# 
+# conda install matplotlib
+# # 
+# # 
+# Chat da google, bacana
+# chat google
+# 
+# conda install scikit-learn
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
